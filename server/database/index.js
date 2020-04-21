1e966b3aec38a098da7d46b17be2062dbd4a1f35
Brandy McArthur <bdm.mcarthur@gmail.com>
2020-04-21 22:55:28 +0100

    Fixed env variable

--- server/database/index.js
+++ server/database/index.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 //your local database url
 //27017 is the default mongoDB port
-const uri = process.env.MONGO;
+const uri = process.env.MONGODB_URI;
 const mongooseConnectionOptions = {
   useNewUrlParser: true,
   useCreateIndex: true,
